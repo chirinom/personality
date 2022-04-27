e01d3780951ccb16dddab7994f95709d853e2051
Miguel <mchirinot@gmail.com>
2022-04-27 03:34:53 +0200

    lading

--- src/components/pages/LandingPage.vue
+++ src/components/pages/LandingPage.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
-    <div class="landing">
-      <span class="header">Are you an introvert <br> or an extrovert? </span>
-      <img class="banner" :src="require('@/assets/images/banner.jpg')" alt="">
+  <div class="landing">
+    <div class="header">
+      <span>Are you an introvert <br> or an extrovert? </span>
+      <button class="header__start-btn" @click="startTest">Start test</button>
     </div>
-    <button class="start-btn" @click="startTest">Start test</button>
+    <img class="banner" :src="require('@/assets/images/banner.jpg')" alt="">
   </div>
 </template>
 
@@ -26,31 +26,34 @@ export default {
   padding: 100px 270px 70px;
 
   & .header {
-    font-size: 87px;
-    font-weight: 900;
-    text-align: center;
-    padding: 65px;
-    line-height: 82px;
+      font-size: 66px;
+      font-weight: 900;
+      text-align: initial;
+      padding: 30px 65px;
+      line-height: 75px;
 
+    &__start-btn {
+      width: 444px;
+      padding: 8px 22px;
+      margin: 44px 0;
+      background-color: lightblue;
+      font-weight: bold;
+      cursor: pointer;
+      border-radius: 6px;
+      border: none;
+      font-size: 24px;
+
+      &:hover {
+        background-color: rgb(62, 182, 221);
+      }
+    }
   }
 
   & .banner {
     width: 630px;
     border-radius: 4px;
+    height: fit-content;
   }
 }
-.start-btn {
-  width: 444px;
-  padding: 22px;
-  background-color: lightblue;
-  font-weight: bold;
-  cursor: pointer;
-  border-radius: 6px;
-  border: none;
-  font-size: 28px;
 
-  &:hover {
-      background-color: rgb(62, 182, 221);
-  }
-}
 </style>
